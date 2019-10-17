ce19165ee812e8066a7df44a6ba22a7b2af03dea
mtimani <timani@etud.insa-toulouse.fr>
2019-10-17 10:44:09 +0200

    TD1

--- TD1/Exercice4/AnnoyingBeep.java
+++ TD1/Exercice4/AnnoyingBeep.java
@@ -13,24 +13,28 @@ public class AnnoyingBeep {
     public AnnoyingBeep() {
         toolkit = Toolkit.getDefaultToolkit();
         timer = new Timer();
-        timer.schedule(new RemindTask(),
-                        5000,        //initial delay
+        timer.scheduleAtFixedRate(new RemindTask(),
+                        5000,     //initial delay
                         1*1000);  //subsequent rate
     }
     
     class RemindTask extends TimerTask {
         int numWarningBeeps = 3;
-
+    
         public void run() {
-            if(numWarningBeeps > 0) {
-                toolkit.beep();
-                System.out.println("Beep!");
-                numWarningBeeps--;
-            } else {
-                toolkit.beep(); 
-                System.out.println("Time's up!");
-                //timer.cancel(); //Not necessary because we call System.exit
-                System.exit(0);   //Stops the AWT thread (and everything else)
+            if (System.currentTimeMillis() - scheduledExecutionTime() >= 5)
+               return;  // Too late; skip this execution.
+            else{
+                if(numWarningBeeps > 0) {
+                    toolkit.beep();
+                    System.out.println("Beep!");
+                    numWarningBeeps--;
+                } else {
+                    toolkit.beep(); 
+                    System.out.println("Time's up!");
+                    //timer.cancel(); //Not necessary because we call System.exit
+                    System.exit(0);   //Stops the AWT thread (and everything else)
+                }
             }
         }
     }
