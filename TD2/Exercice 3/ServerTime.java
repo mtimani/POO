9b9e1ef7dc711d569c115f63874dd2aa7ff496c4
mtimani <timani@etud.insa-toulouse.fr>
2019-10-21 16:51:31 +0200

    TD2

--- TD2/Exercice 3/ServerTime.java
+++ TD2/Exercice 3/ServerTime.java
@@ -1,8 +1,7 @@
 import java.io.*;
 import java.net.*;
-import java.util.Calendar;
-import java.time.*;
 import java.text.*;
+import java.util.*;
 
 public class ServerTime {
 
@@ -17,14 +16,15 @@ public class ServerTime {
     public static void main(String[] arg){
         try {
             ServerTime S = new ServerTime();
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Date date = new Date();
             S.link = S.serverSocket.accept();
             System.out.println("Le serveur est à l'écoute !");
             BufferedReader in = new BufferedReader(new InputStreamReader(S.link.getInputStream()));
             PrintWriter out = new PrintWriter(S.link.getOutputStream(),true);
             System.out.println("Connexion établie avec " + S.link.getInetAddress());
-            System.out.println("Message envoyé : " + timeStamp + "\n");
-            out.println(timeStamp);
+            System.out.println("Message envoyé : " + date + "\n");
+            out.println(date);
             S.link.close();
         }
         catch (Exception e){
